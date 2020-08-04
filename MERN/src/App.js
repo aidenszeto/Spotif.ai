cd2491828cbeadf876cfc78062221123ef466d81
aidenszeto <aszeto35@gmail.com>
2020-08-03 21:10:28 -0700

    front

--- MERN/src/App.js
+++ MERN/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import PlaylistList from "./components/playlist-list.component";
+import CreatePlaylist from "./components/create-playlist.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={PlaylistList} />
+        <Route path="/create" component={CreatePlaylist} />
+      </div>
+    </Router>
   );
 }
+
 
 export default App;

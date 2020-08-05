db71c36f224b657cbe05287e4b76855d84730a58
aidenszeto <aszeto35@gmail.com>
2020-08-04 19:01:50 -0700

    json output width

--- MERN/src/components/home.js
+++ MERN/src/components/home.js
@@ -10,23 +10,21 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
-        <body>
-        enter image url: <input type="text" name="inputImage" id="inputImage"
-            value="" />
-        <button onclick="processImage()">analyze</button>
+        <text>
+        enter image url: <br />
+        <input type="text" name="inputImage" id="inputImage" value="" />
+        <button onclick="processImage()">analyze</button><br /><br />
         <div id="wrapper" style={{width: "1020px", display: "table"}}>
             <div id="jsonOutput" style={{width: "600px", display: "table-cell"}}>
-                JSON:
-                <textarea id="responseTextArea" class="UIInput"></textarea>
+                JSON:<br />
+                <textarea id="responseTextArea" class="UIInput" style={{width: "500px"}}></textarea>
             </div>
             <div id="imageDiv" style={{width: "420px", dispay: "table-cell"}}>
                 image:
                 <img id="sourceImage" width="400" />
             </div>
         </div>
-        </body>
-      </div>
+        </text>
     )
   }
 }

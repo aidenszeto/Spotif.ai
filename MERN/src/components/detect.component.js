43bf52495921e5fcb5e2940bba333c66d5fbacb5
aidenszeto <aszeto35@gmail.com>
2020-08-05 22:00:10 -0700

    console log json

--- MERN/src/components/detect.component.js
+++ MERN/src/components/detect.component.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 export default class Home extends Component {
   constructor(props) {
@@ -13,17 +11,11 @@ export default class Home extends Component {
       }
   }
 
-  onSubmit() {
-    'use strict';
-
+  onSubmit(e) {
     const axios = require('axios').default;
-
     // Add a valid subscription key and endpoint to your environment variables.
     let subscriptionKey = "b2cf0902bf004b89b2a5096c2df5e0e3"
     let endpoint = 'https://spotifai.cognitiveservices.azure.com/face/v1.0/detect'
-
-    var sourceImageUrl = document.getElementById("inputImage").value;
-    document.querySelector("#sourceImage").src = sourceImageUrl;
     // Optionally, replace with your own image URL (for example a .jpg or .png URL).
     let imageUrl = "https://media.beliefnet.com/~/media/photos-with-attribution/people_groups/woman-happy-african-american-smiling-laughing_credit-shutterstock.jpg"
     axios({
@@ -39,19 +31,18 @@ export default class Home extends Component {
         },
         headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey }
     }).then(function (response) {
-        console.log('Status text: ' + response.status)
-        console.log('Status text: ' + response.statusText)
-        console.log()
         //console.log(response.data)
         response.data.forEach((face) => {
           console.log('Face ID: ' + face.faceId)
           console.log('Age: ' + face.faceAttributes.age)
           console.log('Emotion: ' + JSON.stringify(face.faceAttributes.emotion))
           console.log()
+          let json = 'age: ' + face.faceAttributes.age + '\nemotion: ' + JSON.stringify(face.faceAttributes.emotion)
         });
     }).catch(function (error) {
         console.log(error)
     });
+    e.preventDefault();
   }
 
   onChangeURL(e) {
@@ -63,7 +54,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h3>analyze image</h3>
+        <h3>analyze image</h3> <br />
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>link: </label>
@@ -76,6 +67,9 @@ export default class Home extends Component {
           </div>
           <div className="form-group">
             <input type="submit" value="analyze" className="btn btn-primary" />
+          </div> <br />
+          <div>
+            <label>json:</label>
           </div>
         </form>
       </div>

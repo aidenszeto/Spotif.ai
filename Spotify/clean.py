5d4ef132789c331cc139a397750f7684fabd6c23
aidenszeto <aszeto35@gmail.com>
2020-08-12 23:57:58 -0700

    multiplier

--- Spotify/clean.py
+++ Spotify/clean.py
@@ -31,16 +31,17 @@ link = playlists['link']
 id = link[34:56]
 
 # Get track ID for each track in playlist
-tracks = sp.playlist_tracks('1ztgyXj8nULNmEgmPcvWIe', fields='items(track(id))', limit=50)
+tracks = sp.playlist_tracks(id, fields='items(track(id))', limit=50)
 track_list = tracks['items']
 track_scores = 0
 for track in track_list:
     id = track['track']['id']
     features = sp.audio_features(tracks=[id])
-    tempo = features[0]['tempo']/100,2
+    tempo = features[0]['tempo']/100
     energy = features[0]['energy']
-    score = tempo + energy + playlists['emotion']
+    score = (tempo + energy + playlists['emotion'])/3
+    if score < 0.5:
+        score *= 0.7
     track_scores += score
 
-print(track_scores)
-#print(track_scores/len(track_list))
+print(track_scores/len(track_list))

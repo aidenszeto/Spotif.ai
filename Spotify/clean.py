db35b46a0c877deb1ab258849fa61e45a55b83d0
aidenszeto <aszeto35@gmail.com>
2020-08-12 18:42:02 -0700

    spotify api

--- Spotify/clean.py
+++ Spotify/clean.py
@@ -1,18 +1,34 @@
 from dotenv import load_dotenv
 import pymongo, dns, os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import time
+
 
 load_dotenv()
 URI = os.getenv('ATLAS_URI')
+ID = os.getenv('ID')
+SECRET = os.getenv('SECRET')
 
 # Connect to MongoDB database
 my_client = pymongo.MongoClient(URI)
 my_db = my_client['spotifai']
 my_col = my_db['playlists']
 
+# Connect to Spotify API
+client_id = ID
+client_secret = SECRET
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 # Convert items to list and iterate
 playlists = list(my_col.find())
 len = len(playlists)
 for i in range(len):
+    # Get playlist ID from each database entry
     link = playlists[i]['link']
-    id = link[34:]
-    print(id)
+    id = link[34:56]
+
+    tracks = sp.playlist_tracks('1ztgyXj8nULNmEgmPcvWIe', fields='items(track(name))', limit=50)
+    print(tracks)

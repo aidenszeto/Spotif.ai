e32b7dc9af08733b1b174c7ef7b9ceb2c541b0bb
aidenszeto <aszeto35@gmail.com>
2020-08-20 14:42:05 -0700

    mongo uri

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://spotifai:applebanana@cluster0.ybp6o.mongodb.net/spotifai?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;

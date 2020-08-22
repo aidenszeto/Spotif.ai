d632d1d67ba22bd0a990a2f4ffe44893503be128
aidenszeto <aszeto35@gmail.com>
2020-08-21 22:30:35 -0700

    plain text

--- MERN/src/components/navbar.component.js
+++ MERN/src/components/navbar.component.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand"><img src='http://localhost:3000/components/assets/spotify.png'/></Link>
+        <Link to="/" className="navbar-brand">spotif.ai</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
